4104044546b8b223044594ca0e3dd75f7121be00
Velichka Miteva <vmiteva@pop-os.localdomain>
2021-07-25 13:44:30 +0200

    buttons line fixed

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -10,24 +10,18 @@ export default function Homepage() {
         <p className='hello'>HELLO, I am</p>
         <img className='shadow-logo' src={shadow} />
         <img className='original-logo' src={logo} />
-        <p className='text'>"Lorem ipsum dolor sit amet, 
-        consectetur adipiscing elit, sed do eiusmod tempor 
-        incididunt ut labore et dolore magna aliqua. Ut enim 
-        ad minim veniam, quis nostrud exercitation ullamco 
-        laboris nisi ut aliquip ex ea commodo consequat."</p>
         <div class="svg-wrapper">
-          <svg height="55" width="320" xmlns="http://www.w3.org/2000/svg">
+          <svg height="55" width="250" xmlns="http://www.w3.org/2000/svg">
             <rect class="shape" height="55" width="250" />
           </svg>
           <Link to='/projects' className='text-1 btn-1'>PROJECTS</Link>
         </div>
         <div class="svg-wrapper-2">
-          <svg height="55" width="320" xmlns="http://www.w3.org/2000/svg">
+          <svg height="55" width="250" xmlns="http://www.w3.org/2000/svg">
             <rect class="shape" height="55" width="250" />
           </svg>
           <Link to='/services' className='text-1 btn-2'>SERVICES</Link>
         </div>
-        <p className='scroll'><span></span>scroll</p>
       </div>
     )
 }
