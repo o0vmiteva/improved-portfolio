09feaed9d3fba811805f55f26dcf52766045f4ff
Velichka Miteva <vmiteva@pop-os.localdomain>
2021-08-17 15:08:10 +0200

    square animation from top

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -28,12 +28,18 @@ export default function Homepage() {
         </svg>
         <Link to='/services' className='text-1 btn-2'>PROJECTS</Link>
       </div>
-      <div className='square1'></div>
-      <div className='square2'></div>
-      <div className='square3'></div>
-      <div className='square4'></div>
-      <div className='square5'></div>
-
+      <div className='container'>
+        <div className='square'></div>
+          <div className='square'></div>
+            <div className='square'></div>
+              <div className='square'></div>
+                <div className='square'></div>
+                  <div className='square'></div>
+                    <div className='square'></div>
+                      <div className='square'></div>
+                        <div className='square'></div>
+                          <div className='square'></div>
+      </div>
     </div>
   )
 }
